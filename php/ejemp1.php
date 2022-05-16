11b9ae15012a8a3ab949a7ed730f85011204cb7d
bemh1997 <bemh1997@gmail.com>
2022-05-16 15:48:36 -0500

    Agregando todo lo visto hasta el 16 mayo 2022

---
+++ php/ejemp1.php
@@ -0,0 +1,26 @@
+<?php
+	echo "HOLA MUNDO 110422";
+
+	$x = "ESCOM";
+	$y = 2022;
+	$z = 3.1416;
+
+	$r = $x . $y; //concatena
+
+	echo $r; //imprime
+?>
+<!DOCTYPE html>
+<html  lang ="es">
+    <head>
+        <title>PHP</title>
+		<!--archivo: ejercicio_tabla_compleja2.html--> 
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta name="author" content="Bernal Mondragon Humberto"/>
+		<meta name="keywords" content="html, php"/>
+		<meta name="copyright" content="Bernal Mondragon Humberto"/>
+		<meta name="description" content="Ejemplo php"/>
+		<meta name="robots" content="index"/>
+    </head>
+    <body>    
+    </body>
+</html>
